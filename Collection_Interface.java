0a0a173264d8c81938e5455a5d1ac3246e7718ff
Pruthviraj Patole <123588229+pruthvi891@users.noreply.github.com>
2024-07-09 21:23:29 +0530

    Create Collection_Interface.java

---
+++ Collection_Interface.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Collection_Interface {
+    public static void main(String[] args) {
+        // Collection <Integer> coll=new ArrayList<>();
+        // //------Add Method-------
+        // coll.add(10);
+        // coll.add(20);
+
+
+        // //Size Method----------
+        // System.out.println(coll.size());
+
+
+        // //isEmpty() Method
+        //  System.out.println(coll.isEmpty());
+        //  coll.clear();
+
+        //    System.out.println(coll.isEmpty());
+
+
+        //Collection coll = null;
+        //System.out.println(coll.isEmpty());
+        Collection coll = new ArrayList();
+        System.out.println(coll.isEmpty());
+
+        List list = Arrays.asList(1,2,3,4);
+ 
+
+        System.out.println(list);
+
+
+
+    }
+}
