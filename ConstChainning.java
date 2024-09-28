b17f70810ab98018cd473636a9da3a3124f12235
Pruthviraj Patole <123588229+pruthvi891@users.noreply.github.com>
2024-09-28 11:36:48 +0530

    Update ConstChainning.java

--- ConstChainning.java
+++ ConstChainning.java
@@ -30,4 +30,65 @@ Output:
 No argument Constructor
 1 argument Constructor
 2 arguments Constructor
+
+
+
+Example 2: 
+ConstructorChainning
+
+*/
+class demo 
+{
+    int a;
+    int b;
+    int c;
+    demo(int a, int b, int c)
+    {
+
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    //copy Constructor
+    demo(demo d)
+    {
+      
+        this.a=d.a;
+        this.b=d.b;
+        this.c=d.c;
+    }
+    void displayDemo()
+    {
+         System.out.println("Hlo from Display() ----------");
+          System.out.println("a: "+a);
+                    System.out.println("b: "+b);
+                              System.out.println("c: "+c);
+    }
+}
+public class Driver {
+    public static void main(String [] args)
+    {
+        demo d=new demo(10,20,30);
+        d.displayDemo();
+        
+        demo d1=new demo(d);
+        d1.b=100;
+        d1.displayDemo();
+        
+        
+    }
+}
+
+
+/*
+OUTPUT:
+
+Hlo from Display() ----------
+a: 10
+b: 20
+c: 30
+Hlo from Display() ----------
+a: 10
+b: 100
+c: 30
 */
